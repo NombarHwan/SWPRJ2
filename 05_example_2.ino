c0bcae9f99aa983f84be3e532159835069ac4de0
NombarHwan <127061930+NombarHwan@users.noreply.github.com>
2023-09-24 23:13:54 +0900

    Add files via upload

---
+++ 05_example_2.ino
@@ -0,0 +1,17 @@
+#define LED_PIN 7
+bool toggle = 0;
+void setup() {
+  pinMode(LED_PIN, OUTPUT); 
+}
+
+void loop() {
+  digitalWrite(LED_PIN, toggle);
+  delay(1000);
+  for(int i=0; i<=10; i++) {
+    digitalWrite(LED_PIN, !toggle);
+    toggle = !toggle;
+    delay(100);
+  }
+  while(1){
+    }
+  }
